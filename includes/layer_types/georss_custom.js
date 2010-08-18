96589a9d11bd215ab4ebbbb24f7989f00b99b8c8
Alan Palazzolo <github@zzolo.org>
2010-08-18 11:47:58 +0200

    Fixed bugs in GeoRSS layer.

--- includes/layer_types/georss_custom.js
+++ includes/layer_types/georss_custom.js
@@ -13,7 +13,9 @@ Drupal.openlayers.layer.georss_custom = function(title, map, options) {
   if (options.maxExtent !== undefined) {
     options.maxExtent = new OpenLayers.Bounds.fromArray(options.maxExtent) || new OpenLayers.Bounds(-20037508.34, -20037508.34, 20037508.34, 20037508.34);
   }
-  var layer = new OpenLayers.Layer.GeoRSS(title, options.url);
+  options.projection = 'EPSG:'+options.projection;
+  options.sphericalMercator = true;
+  var layer = new OpenLayers.Layer.GeoRSS(title, options.url, options);
   layer.styleMap = styleMap;
   return layer;
 };
